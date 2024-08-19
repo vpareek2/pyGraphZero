1b39786a1a0484da73e6ff0aa81c27720d892276
Veer Pareek <veerpareek12@gmail.com>
2024-08-19 15:23:23 -0700

    GAT finished for now

--- cuda/src/networks/resnet/resnet.cuh
+++ cuda/src/networks/resnet/resnet.cuh
@@ -1,54 +1,50 @@
-#ifndef RESNET_CUH
-#define RESNET_CUH
+#ifndef GAT_CUH
+#define GAT_CUH
 
 #include "../neural_network.h"
 #include <cuda_runtime.h>
 #include <cudnn.h>
+#include <cublas_v2.h>
 #include <torch/torch.h>
+#include <curand.h>
 
 // Model configuration
 typedef struct {
-    int input_channels;
-    int input_height;
-    int input_width;
+    int input_features;
+    int hidden_features;
+    int output_features;
+    int num_heads;
+    int num_layers;
     int num_actions;
-    int num_residual_blocks;
-    int num_filters;
+    int max_nodes;
+    int max_edges;
     float learning_rate;
     float weight_decay;
+    float dropout;
+    float alpha;  // LeakyReLU angle
     int batch_size;
     int epochs;
+    int board_size;  // Added based on usage in gat.cu
 } ModelConfig;
 
-// ResNet model
+// GAT model
 typedef struct {
     // Input block
-    cudnnFilterDescriptor_t input_conv_filter;
-    cudnnTensorDescriptor_t input_bn_mean, input_bn_var;
-    float *input_conv_weights, *input_bn_scale, *input_bn_bias;
+    cudnnTensorDescriptor_t input_descriptor;
+    float *input_weights, *input_bias;
+    cudnnFilterDescriptor_t input_filter;  // Added based on usage in gat.cu
 
-    // Residual blocks
-    cudnnFilterDescriptor_t *res_conv_filters;
-    cudnnTensorDescriptor_t *res_bn_means, *res_bn_vars;
-    float **res_conv_weights, **res_bn_scales, **res_bn_biases;
+    // GAT layers
+    cudnnTensorDescriptor_t *layer_descriptors;
+    float **layer_weights, **layer_biases;
+    float **attention_weights;
+    float **layer_outputs;  // Added based on usage in gat.cu
+    float **attention_scores;  // Added based on usage in gat.cu
 
     // Output block
-    cudnnFilterDescriptor_t value_conv_filter, policy_conv_filter;
-    cudnnTensorDescriptor_t value_bn_mean, value_bn_var, policy_bn_mean, policy_bn_var;
-    float *value_conv_weights, *value_bn_scale, *value_bn_bias;
-    float *policy_conv_weights, *policy_bn_scale, *policy_bn_bias;
-    float *value_fc1_weights, *value_fc1_bias, *value_fc2_weights, *value_fc2_bias;
-    float *policy_fc_weights, *policy_fc_bias;
-
-    // Running mean and variance for batch normalization
-    float *input_bn_mean, *input_bn_var;
-    float **res_bn_means, **res_bn_vars;
-    float *value_bn_mean, *value_bn_var, *policy_bn_mean, *policy_bn_var;
-    
-    // Saved mean and variance for batch normalization
-    float *input_bn_save_mean, *input_bn_save_var;
-    float **res_bn_save_means, **res_bn_save_vars;
-    float *value_bn_save_mean, *value_bn_save_var, *policy_bn_save_mean, *policy_bn_save_var;
+    cudnnTensorDescriptor_t value_descriptor, policy_descriptor;
+    float *value_weights, *value_bias;
+    float *policy_weights, *policy_bias;
 
     // CUDNN handles
     cudnnHandle_t cudnn_handle;
@@ -60,44 +56,61 @@ typedef struct {
     torch::optim::Adam* optimizer;
 
     // Gradient fields
-    float *d_input_conv_weights;
-    float **d_res_conv_weights;
-    float *d_value_conv_weights, *d_policy_conv_weights;
-    float *d_value_fc1_weights, *d_value_fc2_weights;
-    float *d_policy_fc_weights;
+    float *d_input_weights;
+    float **d_layer_weights, **d_attention_weights;
+    float *d_value_weights, *d_policy_weights;
+    float *d_last_layer;  // Added based on usage in gat.cu
+    float **d_layer_outputs;  // Added based on usage in gat.cu
+    float **d_layer_biases;  // Added based on usage in gat.cu
 
     // Workspace for cuDNN
     void *workspace;
     size_t workspace_size;
 
-    // Learning rate and weight decay
-    float learning_rate;
-    float weight_decay;
-} ResNetModel;
+    // Convolution descriptor
+    cudnnConvolutionDescriptor_t conv_descriptor;  // Added based on usage in gat.cu
+
+    // Additional fields
+    float *ones;  // Added based on usage in gat.cu
+} GATModel;
 
 typedef struct {
     INeuralNet base;
-    ResNetModel model;
-} ResNetWrapper;
+    GATModel model;
+} GATWrapper;
 
 // Function prototypes
-INeuralNet* create_resnet_model(const IGame* game);
-static void resnet_init(INeuralNet* self, const IGame* game);
-static void resnet_train(INeuralNet* self, TrainingExample* examples, int num_examples);
-static void resnet_predict(INeuralNet* self, const float* board, float* pi, float* v);
-static void resnet_save_checkpoint(INeuralNet* self, const char* folder, const char* filename);
-static void resnet_load_checkpoint(INeuralNet* self, const char* folder, const char* filename);
-static void resnet_destroy(INeuralNet* self);
+INeuralNet* create_gat_model(const IGame* game);
+static void gat_init(INeuralNet* self, const IGame* game);
+static void gat_train(INeuralNet* self, TrainingExample* examples, int num_examples);
+static void gat_predict(INeuralNet* self, const float* board, float* pi, float* v);
+static void gat_save_checkpoint(INeuralNet* self, const char* folder, const char* filename);
+static void gat_load_checkpoint(INeuralNet* self, const char* folder, const char* filename);
+static void gat_destroy(INeuralNet* self);
 
 // Helper function prototypes
-static void init_model_config(ResNetModel* model, const IGame* game);
-static void init_input_block(ResNetModel* model);
-static void init_residual_blocks(ResNetModel* model);
-static void init_output_block(ResNetModel* model);
-static void init_weights(ResNetModel* model);
-static void prepare_batch(TrainingExample* examples, int num_examples, int batch_size, float** batch_boards, float** batch_pis, float** batch_vs);
-static void forward_resnet(ResNetModel* model, float* batch_boards, float** out_pi, float** out_v);
+static void init_model_config(GATModel* model, const IGame* game);
+static void init_input_block(GATModel* model);
+static void init_gat_layers(GATModel* model);
+static void init_output_block(GATModel* model);
+static void init_weights(GATModel* model);
+static void prepare_batch(TrainingExample* examples, int num_examples, int batch_size,
+                          float* batch_boards, float* batch_pis, float* batch_vs);
+static void forward_gat(GATModel* model, float* batch_boards, float** out_pi, float** out_v);
 static std::pair<float, float> compute_losses(float* target_pi, float* target_v, float* out_pi, float* out_v, int batch_size, int action_size);
-static void backward_resnet(ResNetModel* model, float* batch_boards, float* target_pi, float* target_v, float* out_pi, float* out_v);
-static void adam_update(torch::optim::Adam& optimizer);
-#endif // RESNET_CUH
+static void backward_gat(GATModel* model, float* batch_boards, float* target_pi, float* target_v, float* out_pi, float* out_v);
+
+// CUDA kernel declarations
+__global__ void prepare_batch_kernel(TrainingExample* examples, int num_examples, int* indices,
+                                     float* batch_boards, float* batch_pis, float* batch_vs, int batch_size);
+__global__ void compute_attention(float* inputs, float* weights, float* scores, int num_nodes, int hidden_size, int num_heads);
+__global__ void apply_attention(float* inputs, float* scores, float* outputs, int num_nodes, int hidden_size, int num_heads);
+__global__ void add_bias_activate(float* outputs, float* biases, int num_nodes, int hidden_size);
+__global__ void softmax(float* inputs, int size);
+__global__ void tanh_activate(float* inputs, int size);
+__global__ void softmax_cross_entropy_gradient(float* output, float* target, float* gradient, int batch_size, int num_classes);
+__global__ void mse_gradient(float* output, float* target, float* gradient, int size);
+__global__ void backward_attention(float* inputs, float* scores, float* grad_output, float* grad_weights, float* grad_inputs, int num_nodes, int hidden_size, int num_heads);
+__global__ void backward_activation(float* inputs, float* grad_output, int size);
+
+#endif // GAT_CUH
