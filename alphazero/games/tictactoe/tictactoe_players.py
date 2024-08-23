a6e759f4a9b18fa8911973e34743f07bd3a907a3
Veer Pareek <veerpareek12@gmail.com>
2024-08-23 11:34:39 -0700

    not sure

--- alphazero/games/tictactoe/tictactoe_players.py
+++ alphazero/games/tictactoe/tictactoe_players.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+"""
+Random and Human-ineracting players for the game of TicTacToe.
+
+Author: Evgeny Tyurin, github.com/evg-tyurin
+Date: Jan 5, 2018.
+
+Based on the OthelloPlayers by Surag Nair.
+
+"""
 class RandomPlayer():
     def __init__(self, game):
         self.game = game
@@ -7,41 +16,32 @@ class RandomPlayer():
     def play(self, board):
         a = np.random.randint(self.game.getActionSize())
         valids = self.game.getValidMoves(board, 1)
-        while valids[a] != 1:
+        while valids[a]!=1:
             a = np.random.randint(self.game.getActionSize())
         return a
+
 
 class HumanTicTacToePlayer():
     def __init__(self, game):
         self.game = game
 
     def play(self, board):
+        # display(board)
         valid = self.game.getValidMoves(board, 1)
         for i in range(len(valid)):
             if valid[i]:
                 print(int(i/self.game.n), int(i%self.game.n))
-        while True:
+        while True: 
+            # Python 3.x
             a = input()
-            x, y = [int(x) for x in a.split(' ')]
-            a = self.game.n * x + y if x != -1 else self.game.n ** 2
+            # Python 2.x 
+            # a = raw_input()
+
+            x,y = [int(x) for x in a.split(' ')]
+            a = self.game.n * x + y if x!= -1 else self.game.n ** 2
             if valid[a]:
                 break
             else:
                 print('Invalid')
+
         return a
-
-class GreedyTicTacToePlayer():
-    def __init__(self, game):
-        self.game = game
-
-    def play(self, board):
-        valids = self.game.getValidMoves(board, 1)
-        candidates = []
-        for a in range(self.game.getActionSize()):
-            if valids[a] == 0:
-                continue
-            nextBoard, _ = self.game.getNextState(board, 1, a)
-            score = self.game.getScore(nextBoard, 1)
-            candidates += [(-score, a)]
-        candidates.sort()
-        return candidates[0][1]
